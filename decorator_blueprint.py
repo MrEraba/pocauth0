1c04914c94ad8d914d255f10e6ba4af8c9520bc3
Ivan Almanza <ialmanza@artemishealth.com>
2021-05-04 10:02:53 -0400

    rename files

--- decorator_blueprint.py
+++ decorator_blueprint.py
@@ -4,7 +4,7 @@ from config import config
 from auth import (
     get_token_auth_header, deserialize_token, verify_decorator_key,
 )
-from login_observer import company_subject_factory
+from observers import company_subject_factory
 
 
 decorator_bp = Blueprint('decorator', __name__, url_prefix='/decorate')
